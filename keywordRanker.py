5ca8ab853aa6f28bec7d5adca1a7f15c709a9a4b
Chanuka Ekanayake <chanukasamajith@gmail.com>
2024-11-25 22:09:24 +0530

    initial phase

---
+++ keywordRanker.py
@@ -0,0 +1,9 @@
+class keywordRanker:
+    def __init__(self, keywords, region):
+        self.keyowrds = keywords
+        self.region = region
+
+    
+    
+
+    
---
+++ main.py
@@ -0,0 +1,38 @@
+#------------------------------------------------------------------------------------------------------------------------------
+
+#                                                   -- MoReach: Smarter SEO, Bigger Impact--
+
+# MoReach Phase 1
+# Development Start date: 2024-11-25
+# Main goal - Keyword ranker by city and City ranker by keyword
+# Sub goal - Content Extractor
+
+#------------------------------------------------------------------------------------------------------------------------------
+
+def inputKeyword():
+    keyword = input("\nEnter the keyword: ").strip()
+    return keyword.split(" ")
+
+
+def inputRegion():
+    region = input("\nEnter the region: ").strip()
+    
+
+
+
+
+def main():
+    print("-- MoReach Phase 1 --".center(120))
+
+    print("\n1. Keyword ranker by city")
+    print("2. City ranker by keyword")
+
+    choice = input("\nEnter your choice: ").strip()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
