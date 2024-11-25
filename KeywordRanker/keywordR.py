2c9b4cb76ca53a66f290140473850bdb71173cc8
Chanuka Ekanayake <chanukasamajith@gmail.com>
2024-11-26 01:42:27 +0530

    pytrendsInit

--- KeywordRanker/keywordR.py
+++ KeywordRanker/keywordR.py
@@ -1,5 +1,32 @@
+from pytrends.request import TrendReq
+import pandas as pd
+
 class keywordRanker:
-    def __init__(self, keywords = "Business", region="Colombo"):
+
+    
+
+    def __init__(self, keywords = "Business", region = "Western", time = "today 12-m", gprop = ' ', cat = 0):
         self.keyowrds = keywords
         self.region = region
+        self.time = time
+        self.gprop = gprop
+        self.cat = cat
+
+    def RegionbyKeyword(self):
+        pytrends = TrendReq(hl='en-US', tz=360)
+
+        pytrends.build_payload(
+            self.keyowrds, 
+            cat=self.cat,  #Default category - All
+            timeframe = self.time, 
+            geo="LK", 
+            gprop= self.gprop) #Default gprop - Websearch
+        
+
+        intrest_by_city = pytrends.interest_by_region(resolution='CITY')
+        
+
+        
+
+
 
